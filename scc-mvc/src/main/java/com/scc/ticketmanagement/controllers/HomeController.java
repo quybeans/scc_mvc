cc5362df36381cf25dedbcc5fbe4640d71cb0e25
quybeans <quybeans@gmail.com>
2016-10-28 02:06:02 +0700

    Map trang customer care, sửa profile sidebar

--- scc-mvc/src/main/java/com/scc/ticketmanagement/controllers/HomeController.java
+++ scc-mvc/src/main/java/com/scc/ticketmanagement/controllers/HomeController.java
@@ -158,4 +158,8 @@ public class HomeController {
         return "engagement";
     }
 
+    @RequestMapping("/customercare")
+    public String customercare() {
+        return "customercare";
+    }
 }
