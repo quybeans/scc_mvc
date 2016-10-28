11263ed4916b2931bcb74a341ac69dfd3d8f2b89
quybeans <quybeans@gmail.com>
2016-10-28 10:32:52 +0700

    Update customer care page

--- scc-mvc/src/main/java/com/scc/ticketmanagement/controllers/WebServiceController.java
+++ scc-mvc/src/main/java/com/scc/ticketmanagement/controllers/WebServiceController.java
@@ -115,7 +115,6 @@ public class WebServiceController {
             }
 
             showcomments.add(cmt);
-
         }
         return showcomments;
     }
@@ -185,6 +184,21 @@ public class WebServiceController {
             String username = (String) session.getAttribute("username");
             int userid = userRepository.findUseridByUser(username);
             List<FacebookaccountEntity> accounts = facebookaccountRepository.getFacebookaccountByUserId(userid);
+            return accounts;
+
+        }
+        return null;
+    }
+
+    @RequestMapping("allPageAccount")
+    public List<PageEntity> allPageAccount(HttpServletRequest request)
+    {
+        HttpSession session = request.getSession(false);
+        if (session!=null) {
+            String username = (String) session.getAttribute("username");
+            int userid = userRepository.findUseridByUser(username);
+            UserEntity user = userRepository.findOne(userid);
+            List<PageEntity> accounts = pageRepository.getAllPageByBrandId(user.getBrandid());
             return accounts;
 
         }
