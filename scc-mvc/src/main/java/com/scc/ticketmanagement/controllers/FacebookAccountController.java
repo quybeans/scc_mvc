0c3f5693ab328bec348f2e5e605d9decf92be4d6
giathienpham <giathienpham@gmail.com>
2016-10-29 20:33:03 +0700

    Chia manage page ra làm 2: Page để crawl và page để manage, add page to crawl. Thêm bảng mapping cho user và facebook account sửa lại logic cho việc show facebook account theo user. Now every scc-user can use more than one facebook-account

--- scc-mvc/src/main/java/com/scc/ticketmanagement/controllers/FacebookAccountController.java
+++ scc-mvc/src/main/java/com/scc/ticketmanagement/controllers/FacebookAccountController.java
@@ -1,8 +1,10 @@
 package com.scc.ticketmanagement.controllers;
 
+import com.scc.ticketmanagement.Entities.FacebookaccountEntity;
 import com.scc.ticketmanagement.Entities.UserEntity;
 import com.scc.ticketmanagement.services.FacebookaccountService;
 import com.scc.ticketmanagement.services.UserService;
+import com.scc.ticketmanagement.services.UserfacebookaccountService;
 import com.scc.ticketmanagement.utilities.AccessTokenUtility;
 import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.naming.AuthenticationException;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 /**
  * Created by user on 10/9/2016.
@@ -24,42 +27,43 @@ public class FacebookAccountController {
     FacebookaccountService fbService;
 
     @Autowired
+    UserfacebookaccountService userfacebookaccountService;
+
+    @Autowired
     UserService userService;
 
     @RequestMapping("/facebook-account/index")
-    public String manageFb(HttpServletRequest request, Model model)
-    {
+    public String manageFb(HttpServletRequest request, Model model) {
         HttpSession session = request.getSession(false);
 
-        String username =(String) session.getAttribute("username");
+        String username = (String) session.getAttribute("username");
         UserEntity user = userService.getUserByUsername(username);
-
-        model.addAttribute("facebookAccounts",fbService.getFacebookaccountsByUserID(user.getUserid()));
-
+        List<FacebookaccountEntity> facebookAccounts =  fbService.getFacebookAccountsByUserId(user.getUserid());
+        model.addAttribute("facebookAccounts", facebookAccounts);
 
 
         return "facebook-account/index";
     }
 
     @RequestMapping(value = "/facebook-account/loginViaFacebook", method = RequestMethod.POST)
-    String loginFb(javax.servlet.http.HttpServletRequest request,
+    String loginFb(HttpServletRequest request,
                    @RequestParam("shortLivedToken") String token,
                    @RequestParam("uid") String uid,
                    @RequestParam("fbUsername") String username) {
         String longLivedToken = "";
 
         HttpSession session = request.getSession(false);
-        if (session  == null){
+        if (session == null) {
             return "redirect:/login";
         }
         String currentUser = (String) session.getAttribute("username");
         UserEntity user = userService.getUserByUsername(currentUser);
-        int userId =  user.getUserid();
+        int userId = user.getUserid();
 
         System.out.println("USername: " + username);
         try {
             longLivedToken = AccessTokenUtility.getExtendedAccessToken(token);
-            fbService.createFacebookaccount(uid, longLivedToken, userId,username);
+            fbService.createFacebookaccount(uid, longLivedToken, userId, username);
         } catch (AuthenticationException e) {
             e.printStackTrace();
         }
@@ -68,8 +72,22 @@ public class FacebookAccountController {
     }
 
     @RequestMapping(value = "/facebook-account/deactivateFbAccount", method = RequestMethod.POST)
-    public String deactivate(@RequestParam("facebookAccountId") String accountId){
-        fbService.deactivateFbAccount(accountId);
+    public String deactivate(HttpServletRequest request,
+                             @RequestParam("facebookAccountId") int accountId,
+                             @RequestParam("btnAction") String button) {
+        System.out.println(button);
+        HttpSession session = request.getSession(false);
+        int userId = -1;
+        if (session!=null){
+            String username = (String) session.getAttribute("username");
+            UserEntity user = userService.getUserByUsername(username);
+            userId = user.getUserid();
+        }
+        if (button.equals("Deactivate")) {
+            userfacebookaccountService.deactivate(userId, accountId);
+        } else if (button.equals("Activate")) {
+            userfacebookaccountService.activate(userId, accountId);
+        }
         return "redirect:/facebook-account/index";
     }
 }
